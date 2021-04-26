d6036cf40f2fce5d9d31f18a5702901a4c3a8d21
shika823 <shika823@SOB0473L.staff.uod.otago.ac.nz>
2021-04-26 22:02:13 +1200

    Fixed Schema and started transactionJDBC dao

---
+++ wheel_help/src/main/java/DAO/TransactionJdbcDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Domain.Transaction;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author shika823
+ */
+public class TransactionJdbcDAO implements transactionDAO{
+	
+	private final String url = JdbcConnection.getDefaultConnectionUri();
+
+	@Override
+	public void save(Transaction transaction) {
+	
+String sql = "insert into Transaction () values (?, ?, ?, ?, ?, ?)";
+
+		try (
+			Connection dbCon = JdbcConnection.getConnection(url); 
+			PreparedStatement stmt = dbCon.prepareStatement(sql); 
+		){
+			
+
+                     
+			stmt.executeUpdate();
+			
+		} catch (SQLException ex){
+			throw new DAOException(ex.getMessage(), ex);
+		}
+    }
+}
--- wheel_help/src/main/java/schema.sql
+++ wheel_help/src/main/java/schema.sql
@@ -20,6 +20,16 @@ CREATE TABLE Customer (
 	CONSTRAINT customer_pk PRIMARY KEY (Customer_ID)
 );
  
+
+CREATE TABLE Owner (
+    Owner_ID INTEGER auto_increment,
+    Username VARCHAR(30),
+    Password VARCHAR(30),
+	 Email VARCHAR(30),
+  CONSTRAINT owner_pk PRIMARY KEY (Owner_ID)
+   
+);
+ 
 CREATE TABLE Car (
     Car_ID INTEGER auto_increment,
     Car_NAME VARCHAR(50),
@@ -27,19 +37,11 @@ CREATE TABLE Car (
     Seat_Number INTEGER,
     Hourly_Charge DECIMAL(7,2),
     Location VARCHAR(70),
-    CONSTRAINT car_pk PRIMARY KEY (Car_ID)
+	 owner_Id integer,
+    CONSTRAINT car_pk PRIMARY KEY (Car_ID),
+	 CONSTRAINT car_ownerid_fk FOREIGN KEY (owner_id) REFERENCES Owner (Owner_id)
 );
 
-CREATE TABLE Owner (
-    Owner_ID INTEGER auto_increment,
-    Username VARCHAR(30),
-    Password VARCHAR(30),
-	 Email VARCHAR(30),
-    Car_ID INTEGER,
-    CONSTRAINT owner_pk PRIMARY KEY (Owner_ID),
-    CONSTRAINT owner_carid_fk FOREIGN KEY (Car_ID) REFERENCES Car(Car_ID)
-);
- 
  
 CREATE TABLE Transaction (
     Transaction_ID INTEGER auto_increment,
