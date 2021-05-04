15e1e6efcf20a5ce1b5ccf72efbdf5c7bccc9c8a
shika823 <shika823@SOB0473L.staff.uod.otago.ac.nz>
2021-05-04 15:42:05 +1200

    Started Testing for JDBC DAO

--- wheel_help/src/main/java/DAO/OwnerJdbcDAO.java
+++ wheel_help/src/main/java/DAO/OwnerJdbcDAO.java
@@ -15,7 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class OwnerJdbcDAO implements OwnerDAO {
+public class OwnerJdbcDAO implements ownerDAO {
 
     private String url = JdbcConnection.getDefaultConnectionUri();
 
